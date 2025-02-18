40d1699e2d88a85b37dfd34fd0deb4460c8a539f
Peter Bessenyei <peter.bessenyei@gmail.com>
2025-02-18 08:21:52 +0100

    validate form, reset form after successful validation

--- src/index.js
+++ src/index.js
@@ -8,8 +8,56 @@ document.addEventListener('alpine:init', () => {
         newsletter: true,
 
         showPassword: false,
+        errors: {},
 
-        submitForm() {
+        validateForm() {
+            this.errors = {};
+
+            if (!this.username) {
+                this.errors.username = 'Username is required';
+            }
+
+            if (this.username.length < 3) {
+                this.errors.username = 'Username must be at least 3 characters';
+            }
+
+            if (!this.password) {
+                this.errors.password = 'Password is required';
+            }
+
+            if (this.password.length < 6) {
+                this.errors.password = 'Password must be at least 6 characters';
+            }
+
+            if (this.password !== this.passwordConfirm) {
+                this.errors.passwordConfirm = 'Passwords do not match';
+            }
+
+            if (!this.belt) {
+                this.errors.belt = 'Belt is required';
+            }
+
+            if (!this.bio) {
+                this.errors.bio = 'Bio is required';
+            }
+
+            if (this.bio.length < 10) {
+                this.errors.bio = 'Bio must be at least 10 characters';
+            }
+
+            if (Object.keys(this.errors).length) {
+                return false;
+            }
+
+            return true;
+        },
+
+        submitForm($event) {
+            if (!this.validateForm()) {
+                console.log(this.errors);
+                return;
+            }
+
             console.log({
                 username: this.username,
                 password: this.password,
@@ -18,6 +66,9 @@ document.addEventListener('alpine:init', () => {
                 bio: this.bio,
                 newsletter: this.newsletter
             });
+
+            $event.target.reset();
+            
         }
     }));
 });
